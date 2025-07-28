ab809dfe344b72f25c77938d1b1aec7d642207ba
Ksenia Latvis <ksenia.latvis@gmail.com>
2025-07-28 18:02:31 +0000

    imports updated

--- agents/database_search_agent.py
+++ agents/database_search_agent.py
@@ -13,8 +13,7 @@ import logging
 import json
 import numpy as np
 from typing import Dict, List, Any, Optional, Tuple
-from langchain_community.chat_models import ChatDeepSeek
-from langchain_community.embeddings import DeepSeekEmbeddings
+from langchain_deepseek import ChatDeepSeek, DeepSeekEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from utils.debug_utils import dump_chain_state, log_function_call
 
